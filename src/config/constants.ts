000fccd5fef3bad2edfabd7707ca829eb3cf9a26
June <june@native.org>
2025-04-15 16:00:56 +0800

    chore: update pool address

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const POOL_ADDRESS = '0x32743a10d45481FDC0738896F88CCBd0a638B505'; // replace with actual address
+export const POOL_ADDRESS = '0xB6DaAaa477aCEAC7E45d42420c752AFF3013D058'; // replace with actual address
 export const QUOTE_TOKEN_DECIMALS = 18;
 export const QUOTE_TOKEN_TICKER = "DODO";
 export const BASE_TOKEN_DECIMALS = 18;
